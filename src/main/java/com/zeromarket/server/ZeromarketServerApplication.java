821d667c4bafb894723a4551f59f9bc34598d463
raccoonjeong <rana3782@gmail.com>
2025-11-13 13:34:07 +0900

    initial commit

---
+++ src/main/java/com/zeromarket/server/ZeromarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.zeromarket.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeromarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeromarketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeromarket/server/ZeromarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeromarket.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeromarketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
