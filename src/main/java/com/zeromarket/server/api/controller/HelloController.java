39b105b92ead69ad39cfbce7980655ec3b47c68a
bhs <littleconan@hanmail.net>
2025-11-17 15:49:52 +0900

    fffff

---
+++ src/main/java/com/zeromarket/server/api/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.zeromarket.server.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello git hub";
+    }
+}
