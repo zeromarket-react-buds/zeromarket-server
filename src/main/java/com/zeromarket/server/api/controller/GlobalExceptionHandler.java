ef603fe04898bdc2d75aeea00da5715318bf9ee5
raccoonjeong <rana3782@gmail.com>
2025-11-29 19:18:38 +0900

    [공통] 디버깅용 로그 추가

--- src/main/java/com/zeromarket/server/api/controller/GlobalExceptionHandler.java
+++ src/main/java/com/zeromarket/server/api/controller/GlobalExceptionHandler.java
@@ -4,17 +4,19 @@ import com.zeromarket.server.api.dto.ErrorResponse;
 import com.zeromarket.server.common.enums.ErrorCode;
 import com.zeromarket.server.common.exception.ApiException;
 import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
-
+        log.error(e.getMessage(), e);
         ErrorCode code = e.getErrorCode();
 
         return ResponseEntity
@@ -25,6 +27,7 @@ public class GlobalExceptionHandler {
 //    TODO: e.getMessage()는 log.error 등으로 서버 로그에서만 노출
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
         return ResponseEntity
             .status(500)
             .body(new ErrorResponse("INTERNAL_ERROR", e.getMessage()));
