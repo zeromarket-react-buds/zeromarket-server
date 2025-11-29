7d5e0ea2c2d795809956abcd5df7a9b71193fbd1
kimej214 <kimej21400@gmail.com>
2025-11-30 01:03:26 +0900

    [TR-01-01,TR-01-09] TradeType enum 추가

---
+++ src/main/java/com/zeromarket/server/common/enums/TradeType.java
@@ -0,0 +1,22 @@
+package com.zeromarket.server.common.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum TradeType {
+    /** DELIVERY 택배거래 */
+    DELIVERY("택배거래"),
+
+    /** DIRECT 직거래 */
+    DIRECT("직거래");
+
+    // 필드 추가 (선택 사항: 문자열 설명)
+    private final String description;
+
+    // 생성자
+    TradeType(String description) {
+        this.description = description;
+    }
+}
