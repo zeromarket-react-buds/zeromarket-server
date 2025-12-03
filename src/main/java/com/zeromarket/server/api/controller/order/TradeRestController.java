a2c9e1538c041f980507596911f053f7d955a927
황희원 <82200359+sihoney@users.noreply.github.com>
2025-12-04 01:04:29 +0900

    [TR-01-14] 리뷰 보내기, trades api 변경

--- src/main/java/com/zeromarket/server/api/controller/order/TradeRestController.java
+++ src/main/java/com/zeromarket/server/api/controller/order/TradeRestController.java
@@ -50,7 +50,7 @@ public class TradeRestController {
     }
 
     @Operation(summary = "후기용 거래 상세 조회", description = "")
-    @GetMapping("{tradeId}/reviews")
+    @GetMapping("reviews/{tradeId}")
     public ResponseEntity<TradeReviewInfoDto> getTrade(
         @PathVariable Long tradeId,
         @AuthenticationPrincipal CustomUserDetails userPrincipal
