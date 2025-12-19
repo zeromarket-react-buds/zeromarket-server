fa84e6ebf2f275856b621ea69a3cc6f19e8d4315
kimej214 <kimej21400@gmail.com>
2025-12-19 18:16:21 +0900

    [AI-02-03] name, description 추가

--- src/main/java/com/zeromarket/server/common/enums/OrderStatus.java
+++ src/main/java/com/zeromarket/server/common/enums/OrderStatus.java
@@ -1,11 +1,38 @@
 package com.zeromarket.server.common.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum OrderStatus {
-    PAID,
-    DELIVERY_READY,
-    SHIPPED,
-    DELIVERED,
-    CANCELED;
+    /* PAID 결제완료 */
+    PAID("결제완료"),
+
+    /* DELIVERY_READY 주문확인 */
+    DELIVERY_READY("주문확인"),
+
+    /* SHIPPED 배송중 */
+    SHIPPED("배송중"),
+
+    /* DELIVERED 배송완료 */
+    DELIVERED("배송완료"),
+
+    /* CANCELED 취소 */
+    CANCELED("취소");
+
+    // 필드 추가 (선택 사항: 문자열 설명)
+    private final String description;
+
+    // 생성자
+    OrderStatus(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name();
+    }
+
 
 //    public boolean canTransitTo(OrderStatus next) {
 //        return switch (this) {
